9f98da00d9651b28218e5773fbb2d031f2060445
kwangloc <tqloc1409@gmail.com>
2024-12-06 16:49:57 +0700

    pause consuming msgQueue

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-
 const logger = require('./api/v1/middlewares/logger');
 require('./api/v1/startup/logging.js')(logger); // Logging
 const app = express();
@@ -11,6 +10,8 @@ app.use(cors()); // cho phép tất cả các nguồn gốc
 
 // RabbitMQ
 const { consumeEvents } = require('./rabbitmq/rabbitmqConsumer.js');
+const { cleanQueue } = require('./rabbitmq/cleanQueue.js');
+
 
 require('./api/v1/startup/config.js')(); // Config
 require('./api/v1/startup/routes.js')(app); // Add routes handlers
@@ -18,8 +19,11 @@ require('./api/v1/startup/db.js')(); // Connect to DB
 
 // Environment
 console.log(`app: ${app.get('env')}`); // dev env by default
+
 // RabbitMQ
-consumeEvents();
+// consumeEvents();
+// cleanQueue('job_system_topic', 'auth_service_queue');
+
 // PORT
 const port = process.env.PORT || 3010;
 app.listen(port, () => console.log('Auth service is listening on port', port));
